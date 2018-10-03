fe64d22d320d661a9ddc700bb6a47f46f981de81
salar <salarzardouz@outlook.com>
2018-10-03 13:11:46 +0330

    Add project files.

---
+++ Fahim.Chat.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fahim.Chat.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
